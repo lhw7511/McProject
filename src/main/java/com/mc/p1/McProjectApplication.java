5dc4cfa66b1299735ef15184e829dd20fbc4f455
hyunwook <lhw751@gmail.com>
2020-12-15 13:22:18 +0900

    Initial commit

---
+++ src/main/java/com/mc/p1/McProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mc.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/p1/McProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
